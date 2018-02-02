0836ae79786e0e967e6f0def3bc57cd66d30738e
josephkakou <32803228+josephkakou@users.noreply.github.com>
2018-02-02 14:47:32 +0000

    ok

---
+++ css/page.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>php</title>
+	<meta charset="utf-8">
+</head>
+<body>
+     <p>ce ci est un paragraphe php</p>
+     <?php
+      echo "ceci a ete ecrri en php";
+     ?>
+     <?php echo "ceci es tjr ecri en php";?>
+</body>
+</html>
